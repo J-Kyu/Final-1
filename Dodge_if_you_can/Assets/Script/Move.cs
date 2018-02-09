024a8c8f737a38f994522fbf5c40aca575c552b6
Kyu <wjstkdrb91@naver.com>
2018-02-09 17:16:53 +0900

    new

---
+++ Dodge_if_you_can/Assets/Script/Move.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+    public float speed = 1.0f;
+	public float handle_speed=3.0f;
+	private Rigidbody rb;
+	public float jumpheight=1.0f;
+    public float gravity = -0.98f;
+    public float reverse_speed;
+
+
+   
+
+	// Use this for initialization
+	private void Start () {
+        //reverse_speed = speed;
+		rb = GetComponent<Rigidbody>();
+
+	}
+
+
+    private void Update()
+    {
+        reverse_speed = speed;
+
+        // rb.velocity = transform.up * gravity;
+          
+        if (Input.GetKey(KeyCode.UpArrow))
+        {   
+            rb.velocity = (transform.forward*speed+transform.TransformDirection(0,1,0) *(gravity));//in air the graivity works as (-speed/80)
+
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            
+
+                if(rb.velocity.x<=0 ){
+                rb.velocity = (-transform.forward * speed/2);//going back makes half speed slow   
+             }
+
+          
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime * handle_speed, Space.World);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * handle_speed, Space.World);
+        }
+
+       //In air, i should probably use layer or tag to assign the velocity declining to 0.
+
+        Debug.LogFormat("{0}", rb.velocity.x);
+
+        if (Input.GetKey(KeyCode.R)){
+            transform.position = new Vector3(transform.position.x, 2.0f, transform.position.z);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+
+        Vector3 movedPosition = transform.position;
+
+        if (movedPosition.x > 24)
+        {
+            movedPosition.x = 24;
+        }
+
+
+        if (movedPosition.x < -24)
+        {
+            movedPosition.x = -24;
+        }
+
+        if (movedPosition.z > 24)
+        {
+            movedPosition.z = 24;
+        }                                   //movedPosition.z=Mathf.Clamp(movedPosition.z,-50,+50);
+        if (movedPosition.z < -24)
+        {
+            movedPosition.z = -24;
+        }
+        transform.position = movedPosition;
+
+        
+    
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
+
+
+                if (other.CompareTag("Untagged"))
+                {
+                    rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+                    speed = 0.0f;
+                    reverse_speed = 0.0f;
+                    rb.velocity = (transform.TransformDirection(0, 1, 0) * (gravity));//in air the graivity works as (-speed/80)
+
+                }
+            }
+        }
+
+    
+
+
---
+++ Dodge_if_you_can/Assets/Script/RiseUp.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+/*
+ * after[Instantiate], time of beblow is (seconds) 
+ *      shakingTime=a;
+ *      RockUp=1;
+ *      LastingTime=10;
+ *      RockDown=1;
+ * 
+ * On [Instantiate.cs] give value on "restime" which is sum of above time.
+ * If you sum of above time does not result in "restime", error might be consequence. 
+ * 
+ * 
+ *
+ */
+
+
+
+
+
+public class RiseUp : MonoBehaviour {
+    private float shakingLoadingTime;
+    public float shakingTime=2.0f;
+    private Quaternion originRotation;
+    public float shake_intensity = 0.01f;       //origin value is 0.01f
+    public float shake_decay = 0.002f;          //origin value is 0.002f
+    public float shake_power = 0.2f;            //origin value is 0.2f
+    // Use this for initialization
+    void Start () {
+        originRotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        shakingLoadingTime += Time.deltaTime;
+        //Debug.LogFormat("time:{0}", Time.time);
+        if (shakingLoadingTime < shakingTime)
+        {
+
+            EarthQuake();
+            
+
+        }
+        if (shakingLoadingTime > shakingTime && shakingLoadingTime<shakingTime+1)
+        {
+            RockUp();
+          
+        }
+        if (shakingLoadingTime>shakingTime+1+10)
+        RockDown();
+
+	}
+
+    void RockDown(){
+        transform.Translate(new Vector3(0, -1 * Time.deltaTime, 0));
+    }
+
+    void RockUp(){
+        transform.Translate(new Vector3(0, 1 * Time.deltaTime, 0));
+    }
+
+    void EarthQuake()
+    {
+
+        transform.rotation = new Quaternion(originRotation.x + Random.Range(-shake_intensity, shake_intensity) * shake_power, originRotation.y + Random.Range(-shake_intensity, shake_intensity) * shake_power, originRotation.z + Random.Range(-shake_intensity, shake_intensity) * shake_power, originRotation.w + Random.Range(-shake_intensity, shake_intensity) * shake_power);
+        shake_intensity -= shake_decay;
+    }
+
+   
+}
