be1f41e7c4eb3972882e455fee85f1b63cb1ddd0
J-Kyu <wjstkdrb91@naver.com>
2018-11-19 02:02:59 +0900

    new updates

---
+++ CameraAngle_Test/Assets/_Dodge If you can/Script/InstantiateEnemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class InstantiateEnemy : MonoBehaviour {
+
+    private float enemyLastingTime;
+   
+
+    private float enemyRespawnTime=10f;
+    public GameObject enemyPrefabs;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        enemyLastingTime += Time.deltaTime;
+        if (enemyLastingTime > enemyRespawnTime) {
+            var enemy = Instantiate(enemyPrefabs,transform);
+            enemy.transform.localScale = new Vector3(1,1,1)/20;
+            //enemy.transform.position=ship.transform.position;
+        
+        
+        enemyLastingTime = 0f;
+        }
+    }
+}
